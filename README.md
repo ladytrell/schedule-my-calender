53d84883ee6195cc3629b4e2ff012ed24d3857b6
Lady Trell <antrellkent@gmail.com>
2022-03-15 23:15:40 -0400

    hard coded example html content

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work Day Scheduler Starter Code
+
+## Repository
+[GitHub](https://github.com/ladytrell/.../)
+
+## Active Site
+[Hosted on GitHub ](https://ladytrell.github.io/.../)
+
+
+## Preview Image
+![Preview of web page](./assets/images/demo-image.jpg/)
---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var containerEl = document.querySelector(".container")
+var eventTasks = [];
+
+
+// To Do:  the current day is displayed at the top of the calendar
+// with time blocks for standard business hours
+var loadWorkDay = function () {
+    console.log("loadWorkday");
+};
+
+// To Do: time block is color-coded to indicate whether it is in the past, present, or future
+
+// To Do: click into a time block to enter an event
+// click the save button for that time block
+// the text for that event is saved in local storage
+
+
+loadWorkDay ();
