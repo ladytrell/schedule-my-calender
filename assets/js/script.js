047f1f30f8caba68218685ae5ce56a4e27801339
Lady Trell <antrellkent@gmail.com>
2022-03-16 23:27:25 -0400

    Capture text for tasks and identify the save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,34 @@ var eventTasks = [];
 var workday = 9;
 var startHour = 9;
 
-// To Do:  the current day is displayed at the top of the calendar
-// with time blocks for standard business hours
+// Display the rows for the hours of the day
 var loadWorkDay = function () {
     console.log("loadWorkday");
     var current = moment();
-    console.log(current);
+   // console.log(current);
     
-
+    // Create rows according to startHour and # of hours in workday
     for (var i=startHour; i < (workday + startHour); i++){
         var hour = moment(i, "hh");
-        console.log("i: " + i);
-        console.log("Hour: " + hour);
+        var hourId = moment(hour).format("X");
         var rowEl = document.createElement("div");
         rowEl.classList = "row";
 
         var spanEl = document.createElement("span");
         spanEl.classList = "hour col-1";
-        spanEl.setAttribute("data-hour", hour);
+        spanEl.setAttribute("data-hour", hourId);
         spanEl.textContent = moment(hour).format("hA");
         rowEl.appendChild(spanEl);
 
-        var textareaEl = document.createElement("textarea");
-        textareaEl.classList = "description col-10";
-        textareaEl.setAttribute("data-hour", hour);
-        rowEl.appendChild(textareaEl);
+        var descriptionEL = document.createElement("span");
+        descriptionEL.classList = "description col-10";
+        descriptionEL.setAttribute("data-hour", hourId);
+        rowEl.appendChild(descriptionEL);
 
         var buttonEl = document.createElement("button");
         buttonEl.classList = "saveBtn col-1";
         buttonEl.setAttribute("type", "button");        
-        buttonEl.setAttribute("data-hour", hour);
+        buttonEl.setAttribute("data-hour", hourId);
         var iconEl = document.createElement("span");
         iconEl.classList = "oi oi-lock-locked";
         buttonEl.appendChild(iconEl);
@@ -40,16 +38,48 @@ var loadWorkDay = function () {
 
         containerEl.appendChild(rowEl);
     }
+
+    //auditEventTasks()
 };
 
 // To Do: time block is color-coded to indicate whether it is in the past, present, or future
+
+// To Do: click into a time block to enter an event
+// click the save button for that time block
+// 
+
+//Finds the clicked task space
+$(".time-block").on("click", ".description", function() {
+    console.log(this);
+    var text = $(this).text().trim();
+    var classList = this.className;
+    var dataID = this.getAttribute("data-hour");
+    console.log(dataID);
+
+    //create text area to swap the span
+    var textInput = $("<textarea>")
+    .addClass(classList)
+    .val(text);
+
+    $(this).replaceWith(textInput);
+
+    // Focus on the text area for editing
+    textInput.trigger("focus");
+});
+
+// To Do the text for that event is saved in local storage
+$(".time-block").on("click", ".saveBtn", function() {
+    console.log(this);
+    //Need to get find the textarea
+
+    //If no text erae end
+    //return;
+});
+
+loadWorkDay ();
+
+// To Do:  need to run at app load then every 15 min?
 var auditEventTasks = function () {
     //To get the current date and time, just call moment() with no parameters.
     var now = moment();
 };
-// To Do: click into a time block to enter an event
-// click the save button for that time block
-// the text for that event is saved in local storage
-
-
-loadWorkDay ();
